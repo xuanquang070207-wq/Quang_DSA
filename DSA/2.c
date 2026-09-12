ab4e76ac30aa6c8ea66ab7c0efeaf023543be731
Quang <xuanquang070207@gmail.com>
2026-09-12 21:22:09 +0700

    Check if a subarray with 0 sum exists or not

--- DSA/2.c
+++ DSA/2.c
@@ -1,23 +1,20 @@
+//Check if a subarray with 0 sum exists or not
 #include<stdio.h>
-
-void solve(int nums[], int n) {
+bool solve(int nums[], int n) {
     for (int i=0;i<n;i++) {
         int sum=nums[i];
         for (int j=i+1;j<n;j++) {
             sum+=nums[j];
-            if (sum==0) {
-                printf("[");
-                for (int m=i;m<=j;m++){
-                    printf("%d ",nums[m]);
-                }
-                printf("]\n");
+            if(sum==0){
+                return true;
+            }
             }
         }
-    }
+    return false;
 }
 int main () {
     int nums[]={3, 4, -7, 3, 1, 3, 1, -4, -2, -2 };
     int n=sizeof(nums)/sizeof(nums[0]);
-    solve(nums,n);
+    solve(nums,n)? printf("Subarray with zero-sum exists\n"): printf("Subarray with zero-sum does not exist\n");
     return 0;
 }
